be55d7a93b524150f53c6cc75892b8d027409e43
Edward Sparks <edd_sparks@hotmail.com>
2018-10-16 19:11:10 +0100

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much.";
 
-const token = '8a912225e4fca940704ce1db7b515d1c92a63a74'
+const token = ''
 fetch('https://api.github.com/bubbaspaarx/repos', {
   headers: {
     Authorization: `token ${token}`
